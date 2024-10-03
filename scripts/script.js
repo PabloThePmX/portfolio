be5dc3133ebb8af3fc800a95e77de61a4452fb31
Pablo <pablosarturi1@gmail.com>
2024-10-03 18:14:21 -0300

    fixed sizing top container, sidenav js script

--- scripts/script.js
+++ scripts/script.js
@@ -10,6 +10,42 @@ function typeWriter() {
     }
 }
 
+// var prevScrollpos = window.scrollY;
+// window.onscroll = function() {
+//   var currentScrollPos = window.scrollY;
+//   var nav = document.getElementById("mySidenav");
+//   if (prevScrollpos > currentScrollPos) {
+//     // nav.style.display = "none";
+//   } else {
+//     nav.style.display = "block";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
+
+// https://stackoverflow.com/questions/34372106/check-if-an-element-is-visible-on-screen
+function isVisible(domElement) {
+    return new Promise(resolve => {
+      const o = new IntersectionObserver(([entry]) => {
+        resolve(entry.intersectionRatio === 1);
+        o.disconnect();
+      });
+      o.observe(domElement);
+    });
+}
+
+// https://stackoverflow.com/questions/49432579/await-is-only-valid-in-async-function
+async function verifyElement(){
+    const visible = await isVisible(document.querySelector('#main'));
+    console.log(visible);
+}
+
+
 window.onload = function() {
     typeWriter();
+    isVisible();
 };
+
+//go to the header when the page is reloaded
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
