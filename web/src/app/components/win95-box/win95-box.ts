ad66043c8737e8710dfcaa5ea898f624ed233315
Pablo <pablosarturi1@gmail.com>
2025-12-02 23:53:08 -0300

    start angular project, divide the sections into components

---
+++ web/src/app/components/win95-box/win95-box.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-win95-box',
+  imports: [],
+  templateUrl: './win95-box.html',
+  styleUrl: './win95-box.css'
+})
+export class Win95Box {
+
+}
